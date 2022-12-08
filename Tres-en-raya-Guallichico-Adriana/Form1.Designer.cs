1ef814fba7560da7e4d273415bca17dd054051cb
Adriana Maribel Guallichico Guallichico <amguallichico@pucese.edu.ec>
2022-12-07 19:00:59 -0500

    Tarea#juego

--- Tres-en-raya-Guallichico-Adriana/Form1.Designer.cs
+++ Tres-en-raya-Guallichico-Adriana/Form1.Designer.cs
@@ -48,6 +48,8 @@
             this.groupBox9 = new System.Windows.Forms.GroupBox();
             this.pictureBox9 = new System.Windows.Forms.PictureBox();
             this.lbljugador = new System.Windows.Forms.LinkLabel();
+            this.btnRepetir = new System.Windows.Forms.Button();
+            this.btlSalir = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.groupBox2.SuspendLayout();
@@ -72,9 +74,9 @@
             // 
             this.groupBox1.Controls.Add(this.pictureBox1);
             this.groupBox1.Location = new System.Drawing.Point(24, 26);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
             this.groupBox1.Size = new System.Drawing.Size(231, 166);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
@@ -84,7 +86,7 @@
             this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
             this.pictureBox1.Image = global::Tres_en_raya_Guallichico_Adriana.Properties.Resources.nada;
             this.pictureBox1.Location = new System.Drawing.Point(8, 14);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(215, 145);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -96,9 +98,9 @@
             // 
             this.groupBox2.Controls.Add(this.pictureBox2);
             this.groupBox2.Location = new System.Drawing.Point(296, 26);
-            this.groupBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4);
             this.groupBox2.Size = new System.Drawing.Size(233, 161);
             this.groupBox2.TabIndex = 1;
             this.groupBox2.TabStop = false;
@@ -108,7 +110,7 @@
             this.pictureBox2.Cursor = System.Windows.Forms.Cursors.Hand;
             this.pictureBox2.Image = global::Tres_en_raya_Guallichico_Adriana.Properties.Resources.estrella;
             this.pictureBox2.Location = new System.Drawing.Point(8, 14);
-            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(217, 140);
             this.pictureBox2.TabIndex = 1;
@@ -120,9 +122,9 @@
             this.groupBox3.Controls.Add(this.pictureBox3);
             this.groupBox3.Cursor = System.Windows.Forms.Cursors.Hand;
             this.groupBox3.Location = new System.Drawing.Point(571, 26);
-            this.groupBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox3.Margin = new System.Windows.Forms.Padding(4);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox3.Padding = new System.Windows.Forms.Padding(4);
             this.groupBox3.Size = new System.Drawing.Size(264, 156);
             this.groupBox3.TabIndex = 2;
             this.groupBox3.TabStop = false;
@@ -132,7 +134,7 @@
             this.pictureBox3.Cursor = System.Windows.Forms.Cursors.Hand;
             this.pictureBox3.Image = global::Tres_en_raya_Guallichico_Adriana.Properties.Resources.estrella;
             this.pictureBox3.Location = new System.Drawing.Point(8, 14);
-            this.pictureBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(4);
             this.pictureBox3.Name = "pictureBox3";
             this.pictureBox3.Size = new System.Drawing.Size(248, 132);
             this.pictureBox3.TabIndex = 1;
@@ -143,9 +145,9 @@
             // 
             this.groupBox4.Controls.Add(this.pictureBox4);
             this.groupBox4.Location = new System.Drawing.Point(16, 199);
-            this.groupBox4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox4.Margin = new System.Windows.Forms.Padding(4);
             this.groupBox4.Name = "groupBox4";
-            this.groupBox4.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox4.Padding = new System.Windows.Forms.Padding(4);
             this.groupBox4.Size = new System.Drawing.Size(239, 153);
             this.groupBox4.TabIndex = 3;
             this.groupBox4.TabStop = false;
@@ -155,7 +157,7 @@
             this.pictureBox4.Cursor = System.Windows.Forms.Cursors.Hand;
             this.pictureBox4.Image = global::Tres_en_raya_Guallichico_Adriana.Properties.Resources.estrella;
             this.pictureBox4.Location = new System.Drawing.Point(8, 2);
-            this.pictureBox4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox4.Margin = new System.Windows.Forms.Padding(4);
             this.pictureBox4.Name = "pictureBox4";
             this.pictureBox4.Size = new System.Drawing.Size(223, 143);
             this.pictureBox4.TabIndex = 1;
@@ -166,9 +168,9 @@
             // 
             this.groupBox5.Controls.Add(this.pictureBox5);
             this.groupBox5.Location = new System.Drawing.Point(296, 194);
-            this.groupBox5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox5.Margin = new System.Windows.Forms.Padding(4);
             this.groupBox5.Name = "groupBox5";
-            this.groupBox5.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox5.Padding = new System.Windows.Forms.Padding(4);
             this.groupBox5.Size = new System.Drawing.Size(240, 150);
             this.groupBox5.TabIndex = 4;
             this.groupBox5.TabStop = false;
@@ -178,7 +180,7 @@
             this.pictureBox5.Cursor = System.Windows.Forms.Cursors.Hand;
             this.pictureBox5.Image = global::Tres_en_raya_Guallichico_Adriana.Properties.Resources.estrella;
             this.pictureBox5.Location = new System.Drawing.Point(8, 7);
-            this.pictureBox5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox5.Margin = new System.Windows.Forms.Padding(4);
             this.pictureBox5.Name = "pictureBox5";
             this.pictureBox5.Size = new System.Drawing.Size(232, 135);
             this.pictureBox5.TabIndex = 1;
@@ -189,9 +191,9 @@
             // 
             this.groupBox6.Controls.Add(this.pictureBox6);
             this.groupBox6.Location = new System.Drawing.Point(571, 190);
-            this.groupBox6.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox6.Margin = new System.Windows.Forms.Padding(4);
             this.groupBox6.Name = "groupBox6";
-            this.groupBox6.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox6.Padding = new System.Windows.Forms.Padding(4);
             this.groupBox6.Size = new System.Drawing.Size(264, 162);
             this.groupBox6.TabIndex = 5;
             this.groupBox6.TabStop = false;
@@ -201,7 +203,7 @@
             this.pictureBox6.Cursor = System.Windows.Forms.Cursors.Hand;
             this.pictureBox6.Image = global::Tres_en_raya_Guallichico_Adriana.Properties.Resources.estrella;
             this.pictureBox6.Location = new System.Drawing.Point(8, 10);
-            this.pictureBox6.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox6.Margin = new System.Windows.Forms.Padding(4);
             this.pictureBox6.Name = "pictureBox6";
             this.pictureBox6.Size = new System.Drawing.Size(248, 138);
             this.pictureBox6.TabIndex = 1;
@@ -212,9 +214,9 @@
             // 
             this.groupBox7.Controls.Add(this.pictureBox7);
             this.groupBox7.Location = new System.Drawing.Point(16, 370);
-            this.groupBox7.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox7.Margin = new System.Windows.Forms.Padding(4);
             this.groupBox7.Name = "groupBox7";
-            this.groupBox7.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox7.Padding = new System.Windows.Forms.Padding(4);
             this.groupBox7.Size = new System.Drawing.Size(251, 169);
             this.groupBox7.TabIndex = 6;
             this.groupBox7.TabStop = false;
@@ -224,7 +226,7 @@
             this.pictureBox7.Cursor = System.Windows.Forms.Cursors.Hand;
             this.pictureBox7.Image = global::Tres_en_raya_Guallichico_Adriana.Properties.Resources.estrella;
             this.pictureBox7.Location = new System.Drawing.Point(0, 7);
-            this.pictureBox7.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox7.Margin = new System.Windows.Forms.Padding(4);
             this.pictureBox7.Name = "pictureBox7";
             this.pictureBox7.Size = new System.Drawing.Size(235, 154);
             this.pictureBox7.TabIndex = 1;
@@ -235,9 +237,9 @@
             // 
             this.groupBox8.Controls.Add(this.pictureBox8);
             this.groupBox8.Location = new System.Drawing.Point(305, 370);
-            this.groupBox8.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox8.Margin = new System.Windows.Forms.Padding(4);
             this.groupBox8.Name = "groupBox8";
-            this.groupBox8.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox8.Padding = new System.Windows.Forms.Padding(4);
             this.groupBox8.Size = new System.Drawing.Size(239, 169);
             this.groupBox8.TabIndex = 7;
             this.groupBox8.TabStop = false;
@@ -247,7 +249,7 @@
             this.pictureBox8.Cursor = System.Windows.Forms.Cursors.Hand;
             this.pictureBox8.Image = global::Tres_en_raya_Guallichico_Adriana.Properties.Resources.estrella;
             this.pictureBox8.Location = new System.Drawing.Point(0, 7);
-            this.pictureBox8.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox8.Margin = new System.Windows.Forms.Padding(4);
             this.pictureBox8.Name = "pictureBox8";
             this.pictureBox8.Size = new System.Drawing.Size(231, 146);
             this.pictureBox8.TabIndex = 1;
@@ -258,9 +260,9 @@
             // 
             this.groupBox9.Controls.Add(this.pictureBox9);
             this.groupBox9.Location = new System.Drawing.Point(571, 370);
-            this.groupBox9.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox9.Margin = new System.Windows.Forms.Padding(4);
             this.groupBox9.Name = "groupBox9";
-            this.groupBox9.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox9.Padding = new System.Windows.Forms.Padding(4);
             this.groupBox9.Size = new System.Drawing.Size(264, 161);
             this.groupBox9.TabIndex = 8;
             this.groupBox9.TabStop = false;
@@ -270,7 +272,7 @@
             this.pictureBox9.Cursor = System.Windows.Forms.Cursors.Hand;
             this.pictureBox9.Image = global::Tres_en_raya_Guallichico_Adriana.Properties.Resources.estrella;
             this.pictureBox9.Location = new System.Drawing.Point(8, 7);
-            this.pictureBox9.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox9.Margin = new System.Windows.Forms.Padding(4);
             this.pictureBox9.Name = "pictureBox9";
             this.pictureBox9.Size = new System.Drawing.Size(248, 146);
             this.pictureBox9.TabIndex = 1;
@@ -290,11 +292,33 @@
             this.lbljugador.Text = "Turno del jugador";
             this.lbljugador.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
             // 
+            // btnRepetir
+            // 
+            this.btnRepetir.Location = new System.Drawing.Point(897, 434);
+            this.btnRepetir.Name = "btnRepetir";
+            this.btnRepetir.Size = new System.Drawing.Size(119, 47);
+            this.btnRepetir.TabIndex = 10;
+            this.btnRepetir.Text = "Repetir";
+            this.btnRepetir.UseVisualStyleBackColor = true;
+            this.btnRepetir.Click += new System.EventHandler(this.btnRepetir_Click);
+            // 
+            // btlSalir
+            // 
+            this.btlSalir.Location = new System.Drawing.Point(905, 127);
+            this.btlSalir.Name = "btlSalir";
+            this.btlSalir.Size = new System.Drawing.Size(102, 45);
+            this.btlSalir.TabIndex = 11;
+            this.btlSalir.Text = "Salir";
+            this.btlSalir.UseVisualStyleBackColor = true;
+            this.btlSalir.Click += new System.EventHandler(this.btlSalir_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.btlSalir);
+            this.Controls.Add(this.btnRepetir);
             this.Controls.Add(this.lbljugador);
             this.Controls.Add(this.groupBox9);
             this.Controls.Add(this.groupBox8);
@@ -305,7 +329,7 @@
             this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -354,6 +378,8 @@
         private System.Windows.Forms.GroupBox groupBox9;
         private System.Windows.Forms.PictureBox pictureBox9;
         private System.Windows.Forms.LinkLabel lbljugador;
+        private System.Windows.Forms.Button btnRepetir;
+        private System.Windows.Forms.Button btlSalir;
     }
 }
 
--- Tres-en-raya-Guallichico-Adriana/Form1.cs
+++ Tres-en-raya-Guallichico-Adriana/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TrackBar;
 
 namespace Tres_en_raya_Guallichico_Adriana
 {
@@ -15,6 +16,8 @@ namespace Tres_en_raya_Guallichico_Adriana
         private string jugador1 = "JUAN";
         private string jugador2 = "JOSE";
         private bool turno = true;
+        private int Ganar = 0;
+        private int Movimientos = 0;
 
         private int[] jugadas = { 0, 0, 0, 0, 0, 0, 0, 0, 0};
         
@@ -50,6 +53,113 @@ namespace Tres_en_raya_Guallichico_Adriana
                 Console.WriteLine("{0}", valor);
             }
         }
+        private void desactivarJuego()
+        {
+            pictureBox1.Enabled = false;
+            pictureBox2.Enabled = false;
+            pictureBox3.Enabled = false;
+            pictureBox4.Enabled = false;
+            pictureBox5.Enabled = false;
+            pictureBox6.Enabled = false;
+            pictureBox7.Enabled = false;
+            pictureBox8.Enabled = false;
+        }
+        private void ganar()
+        {
+           //horizontal
+            if (jugadas[0] == 1 && jugadas[1] == 1 && jugadas[2] == 1)
+            {
+                Ganar = 1;
+            }
+            else if (jugadas[3] == 1 && jugadas[4] == 1 && jugadas[5] == 1)
+            {
+                Ganar = 1;
+            }
+            else if (jugadas[6] == 1 && jugadas[7] == 1 && jugadas[8] == 1)
+            {
+                Ganar = 1;
+            }
+            //Vertical
+            else if (jugadas[0] == 1 && jugadas[3] == 1 && jugadas[6] == 1)
+            {
+                Ganar = 1;
+            }
+            else if (jugadas[1] == 1 && jugadas[4] == 1 && jugadas[7] == 1)
+            {
+                Ganar = 1;
+            }
+            else if (jugadas[2] == 1 && jugadas[5] == 1 && jugadas[8] == 1)
+            {
+                Ganar = 1;
+            }
+            //Diagonal
+            else if (jugadas[0] == 1 && jugadas[4] == 1 && jugadas[8] == 1)
+            {
+                Ganar = 1;
+            }
+            else if (jugadas[6] == 1 && jugadas[4] == 1 && jugadas[2] == 1)
+            {
+                Ganar = 1;
+            }
+            //jugador 2
+            //horizontal
+            else if (jugadas[0] == 2 && jugadas[1] == 2 && jugadas[2] == 2)
+            {
+                Ganar = 2;
+            }
+            else if (jugadas[3] == 2 && jugadas[4] == 2 && jugadas[5] == 2)
+            {
+                Ganar = 2;
+            }
+            else if (jugadas[6] == 2 && jugadas[7] == 2 && jugadas[8] == 2)
+            {
+                Ganar = 2;
+            }
+            //vertical
+            else if (jugadas[0] == 2 && jugadas[3] == 2 && jugadas[6] == 2)
+            {
+                Ganar = 2;
+            }
+            else if (jugadas[1] == 2 && jugadas[4] == 2 && jugadas[7] == 2)
+            {
+                Ganar = 2;
+            }
+            else if (jugadas[2] == 2 && jugadas[5] == 2 && jugadas[8] == 2)
+            {
+                Ganar = 2;
+            }
+            //Diagonal
+            else if (jugadas[0] == 2 && jugadas[4] == 2 && jugadas[8] == 2)
+            {
+                Ganar = 2;
+            }
+            else if (jugadas[6] == 2 && jugadas[4] == 2 && jugadas[2] == 2)
+            {
+                Ganar = 2;
+            }
+            if (Ganar == 1)
+            {
+                MessageBox.Show($"El jugador {jugador1} ha ganado.");
+                desactivarJuego();
+                btnRepetir.Enabled = true;
+                return;
+            }
+            else if (Ganar == 2)
+            {
+                MessageBox.Show($"El jugador {jugador2} ha ganado.");
+                desactivarJuego();
+                btnRepetir.Enabled = true;
+                return;
+            }
+            if (Movimientos == 9)
+            {
+                MessageBox.Show($"Empate.");
+                desactivarJuego();
+                btnRepetir.Enabled = true;
+                return;
+            }
+        }
+
         private void desactivarPicture(PictureBox picture)
         {
             picture.Enabled = false;
@@ -132,6 +242,19 @@ namespace Tres_en_raya_Guallichico_Adriana
             desactivarPicture(pictureBox9);
             turno = !turno;
             quienjuega(turno);
+        }
+       
+
+        private void btnRepetir_Click(object sender, EventArgs e)
+        {
+            Form1 Juego = new Form1();
+            Juego.Show();
+            this.Dispose(false);
+        }
+
+        private void btlSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
